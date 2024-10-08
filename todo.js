8919836ffd60b62fe9db9f5e71c1ca88ac40c6a1
Shikhar Khandelwal <73191595+skykunnu@users.noreply.github.com>
2024-10-08 15:53:29 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -6,8 +6,13 @@ let BgColor=[
     {backgroundColor:"#F9D689", color:"black"},
     {backgroundColor:"#F6E9B2" , color:"black"},
     {backgroundColor:"#FFE0B6" , color:"black"}
-
 ];
+
+let ItemArr=[];
+let count=0;
+let flag=false;
+let currentEditPara=null;
+let changedObjectTask;
 
 addbtn.addEventListener('click', Tasks);
 
@@ -16,7 +21,24 @@ function Tasks(){
     if(Input.value===""){
         alert('Please Enter the task !')
     }
+
     else{
+
+        if(flag){
+            currentEditPara.innerHTML=Input.value;
+            ItemArr=ItemArr.map((item)=>{
+                if(item.task===changedObjectTask){
+                    item.task=Input.value
+                }
+                return item
+                
+             })
+            flag=false;
+            currentEditPara=null;
+            Input.value='';
+        }
+
+     else{
         let para=document.createElement('p');
     let delBtn=document.createElement('span');
     let deli=document.createElement('i')
@@ -25,9 +47,15 @@ function Tasks(){
     let ItemDiv=document.createElement('div');
     let btnDiv=document.createElement('div');
     para.innerHTML=Input.value;
+
+    let itemObj={
+        id:++count,
+        task:para.innerHTML
+    }
     
     ItemDiv.classList='items'
     btnDiv.classList='btns'
+    
     let Colors=BgColor[BGColor()]
 
     ItemDiv.style.backgroundColor=Colors.backgroundColor;
@@ -43,38 +71,42 @@ function Tasks(){
     btnDiv.append(delBtn,editBtn);
     ItemDiv.append(para,btnDiv);
     todoCont.append(ItemDiv)
-
-
     
-    Input.value=''
-
     delBtn.addEventListener('click',function(){
-        Delete(ItemDiv);
+        Delete(itemObj.id,ItemDiv);
     });
-
-    editBtn.addEventListener('click',function(){
-         edit(para,ItemDiv);
-    })
-    }
-
     
-
+    editBtn.addEventListener('click',function(){
+        edit(para,itemObj.task);
+    })
+    
+    ItemArr.push(itemObj);
+    Input.value='';
+     }
+    }
 }
 
-function Delete(ItemDiv){
+
+
+
+function Delete(ID,ItemDiv){
+    ItemArr=ItemArr.filter(Arrayitem=>Arrayitem.id!==ID)
     ItemDiv.remove();
+    console.log(ItemArr)
     }
 
- function edit(para,ItemDiv){
 
-   Input.value=para.innerHTML;
-   para.innerHTML=""
 
-   addbtn.addEventListener('click',function(){
-    para.innerHTML=ItemDiv.replaceWith(Input.value);
-   })
+ function edit(para,Task){
+
+     Input.value=para.innerHTML;
+     para.innerHTML=""
+    changedObjectTask=Task;
+    flag=true;
+    currentEditPara=para;
  }
 
+ 
 function BGColor(){
     return Math.floor(Math.random()*BgColor.length);
 }
